3ed8bcec2e33de03101f0b87ce7c6cd50b6ff5bc
Knrc <kontakt.mikey@gmail.com>
2023-09-06 10:30:26 +0200

    Basic Variables, Function and Function Calls

--- 2A_Introduction_CSharp/Program.cs
+++ 2A_Introduction_CSharp/Program.cs
@@ -1,3 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿
 
-Console.WriteLine("Hello, World!");
+int age = 21;
+string name = "Mikey";
+bool tired = true;
+float grade = 2.1f;
+
+
+Awawa(name);
+
+
+void Awawa(string textToSay)
+{
+    Console.WriteLine(textToSay);
+}
